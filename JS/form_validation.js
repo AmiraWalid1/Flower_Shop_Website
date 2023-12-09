7560ae5ccb7b74900001acee4fb07362d63da134
Heba Yasser <hebay99@gmail.com>
2023-12-09 14:19:18 +0200

    form validation

--- JS/form_validation.js
+++ JS/form_validation.js
@@ -2,8 +2,10 @@
 let form = document.querySelector("form");
 let emailInput = document.getElementById('emailInput');
 let passwordInput = document.getElementById('passwordInput');
-let messageBox = document.getElementById('messageBox');
-let messageText = document.getElementById('messageText');
+let error1 = document.querySelector('.error1');
+let error2 = document.querySelector('.error2');
+let error3 = document.querySelector('.error3');
+let btn = document.querySelector(".btn");
 
 
 form.addEventListener(('submit'), (e)=> {
@@ -13,24 +15,19 @@ form.addEventListener(('submit'), (e)=> {
     let validEmail = /\b[A-Za-z0-9._-]+@(gmail|yahoo).com/;
     let validPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[\d@$!%*?&])[a-zA-Z\d]{8,10}$/;
 
-    if (!email.match(validEmail)) {
-        showMsg('Invalid email format. Please enter a valid email address.');
+    if (!email.match(validEmail) && !(password.match(validPassword))) {
+        error3.style.display = "block";
     } else if (!(password.match(validPassword))) {
-        showMsg('Password must contain at least 8 characters, including uppercase, lowercase, and a number or special character.');
-    } else if (!email.match(validEmail) && !(password.match(validPassword))){
-        showMsg('Invalid email and password Please try again.');
+        error2.style.display = "block";
+    } else if (!email.match(validEmail)) {
+        error1.style.display = "block";
     } else {
         window.location.href = "home.html";
     }
-});
 
-function showMsg(msg) {
-    messageText.textContent = msg;
-    form.style.display = 'none';
-    messageBox.style.display = 'block';
-}
-
-function closeMsg() {
-    form.style.display = 'block';
-    messageBox.style.display = 'none';
-}
+    btn.onclick = function () {
+        error1.style.display = "none";
+        error2.style.display = "none";
+        error3.style.display = "none";
+    }
+});   
